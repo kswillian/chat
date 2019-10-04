f7a277fc500845b09f883c5f724e6c69199b0518
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-10-04 00:29:57 -0300

    first commit

---
+++ app/src/main/java/santos/williankaminski/chat/activity/LoginActivity.java
@@ -0,0 +1,21 @@
+package santos.williankaminski.chat.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import santos.williankaminski.chat.R;
+
+/**
+ * @author Willian Kaminski dos santos
+ * @since 04-10-2019
+ * @version 0.0.1
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
