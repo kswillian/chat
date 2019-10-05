7732690034d6bcc2b2f4520a3fa7eef0b6f156d0
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-10-05 00:17:40 -0300

    Parametrizando a classe de configuração do Firebase

---
+++ app/src/main/java/santos/williankaminski/chat/config/FirebaseConf.java
@@ -0,0 +1,32 @@
+package santos.williankaminski.chat.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * @author Willian Kaminski dos santos
+ * @since 05-10-2019
+ * @version 0.0.1
+ */
+public class FirebaseConf {
+
+    private static DatabaseReference databaseReference;
+    private static FirebaseAuth firebaseAuth;
+
+    // retorna a instancia do FirebaseDatabase
+    public static DatabaseReference getFirenaseDatabase(){
+        if(databaseReference == null){
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+        return databaseReference;
+    }
+
+    // retorna a instancia do FirebaseAuth
+    public static FirebaseAuth  getFirebaseAuth(){
+        if(firebaseAuth == null){
+            firebaseAuth = FirebaseAuth.getInstance();
+        }
+        return firebaseAuth;
+    }
+}
